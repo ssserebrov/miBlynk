b16e535f5df87604315941603ec0d74023adb4f2
Andrey Serebrov <andser45@mail.ru>
2018-04-19 16:34:07 +0500

    add xiaomi part

--- miGateway/app.js
+++ miGateway/app.js
@@ -1,12 +1,92 @@
-var BlynkLib = require('blynk-library');
-var blynk = new BlynkLib.Blynk('7bb7485f6eba41a0a36de66a90ed8ea1');
-var v1 = new blynk.VirtualPin(1);
-var v20 = new blynk.VirtualPin(20);
-var str = "sdfsdfs";
-v20.on('write', function (param) {
-    console.log('V1:', param);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const BlynkLib = require('blynk-library');
+const miio = require('miio');
+// Gateway stuff
+let gateway;
+let plug;
+let button;
+let sensorHT;
+// Blynk stuff
+let blynk;
+let v20;
+let testPin;
+const initGateway = () => __awaiter(this, void 0, void 0, function* () {
+    gateway = yield miio.device({ address: '192.168.1.70' });
+    console.log(gateway);
+    plug = gateway.child('miio:158d00020f23d5');
+    button = gateway.child('miio:158d00020fecb9');
+    sensorHT = gateway.child('miio:158d0001c2a921');
+    console.log(plug);
+    for (const child of gateway.children()) {
+        console.log(child.id);
+        console.log(child);
+        //if (child.matches('cap:actions')) {
+        //    const actions = await child.actions();
+        //    console.log(actions);
+        //}
+        if (child.matches('cap:temperature')) {
+            const temperature = yield child.temperature();
+            console.log('Temperature:', temperature.celsius);
+        }
+        //plug = gateway.child(child.id);
+    }
+    //console.log(plug);
 });
-v20.on('read', function () {
-    v20.write(new Date().getSeconds());
+const plugTurn = (on) => __awaiter(this, void 0, void 0, function* () {
+    if (on) {
+        console.log("plug.turnOn();");
+        plug.turnOn();
+    }
+    else {
+        console.log("plug.turnOff();");
+        plug.turnOff();
+    }
+});
+const initBlynk = () => __awaiter(this, void 0, void 0, function* () {
+    blynk = yield new BlynkLib.Blynk('7bb7485f6eba41a0a36de66a90ed8ea');
+    v20 = yield new blynk.VirtualPin(20);
+    testPin = yield new blynk.VirtualPin(17);
+});
+const testBlynk = () => __awaiter(this, void 0, void 0, function* () {
+    console.log("testBlynk");
+    testPin.write(1);
+});
+const initEvents = () => __awaiter(this, void 0, void 0, function* () {
+    console.log("->initEvents");
+    v20.on('write', function (param) {
+        console.log('V20:', param);
+        if (param == 0)
+            plugTurn(false);
+        if (param == 1)
+            plugTurn(true);
+    });
+    v20.on('read', function () {
+        v20.write(new Date().getSeconds());
+    });
+    blynk.on('connect', function () {
+        console.log("Blynk ready.");
+        testBlynk();
+    });
+    console.log("initEvents->");
+    // button.on('action:click', event => console.log('Action', event.action, 'with data', event.data));
+    // sensorHT.on('temperatureChanged', temp => console.log('Temp changed to:', temp));
+});
+const run = () => __awaiter(this, void 0, void 0, function* () {
+    //await initGateway();
+    yield initBlynk();
+    //await testBlynk();
+    yield initEvents();
+    console.log("run->");
+    //await plugTurnOn();
+});
+run().catch(err => {
+    console.log(err);
 });
 //# sourceMappingURL=app.js.map
--- miGateway/app.ts
+++ miGateway/app.ts
@@ -1,16 +1,100 @@
-﻿let BlynkLib = require('blynk-library');
+﻿const BlynkLib = require('blynk-library');
+const miio = require('miio');
 
-let blynk : any = new BlynkLib.Blynk('7bb7485f6eba41a0a36de66a90ed8ea1');
-let v1 : any = new blynk.VirtualPin(1);
-let v20: any = new blynk.VirtualPin(20);
+// Gateway stuff
+let gateway: any;
+let plug: any;
+let button: any;
+let sensorHT: any;
 
-let str: string = "sdfsdfs";
+// Blynk stuff
+let blynk: any;
+let v20: any;
+let testPin: any;
+
+const initGateway = async () => {
+    gateway = await miio.device({ address: '192.168.1.70' })
+    console.log(gateway);
+
+    plug = gateway.child('miio:158d00020f23d5');
+    button = gateway.child('miio:158d00020fecb9');
+    sensorHT = gateway.child('miio:158d0001c2a921');
+
+    console.log(plug);
+    for (const child of gateway.children()) {
+        console.log(child.id);
+        console.log(child);
+        //if (child.matches('cap:actions')) {
+        //    const actions = await child.actions();
+        //    console.log(actions);
+        //}
+        if (child.matches('cap:temperature')) {
+            const temperature = await child.temperature();
+            console.log('Temperature:', temperature.celsius);
+        }
+        //plug = gateway.child(child.id);
+    }
+    //console.log(plug);
+}
+
+const plugTurn = async (on: boolean) => {
+    if (on) {
+        console.log("plug.turnOn();");
+        plug.turnOn();
+    }
+    else {
+        console.log("plug.turnOff();");
+        plug.turnOff();
+    }
+}
+
+const initBlynk = async () => {
+    blynk = await new BlynkLib.Blynk('7bb7485f6eba41a0a36de66a90ed8ea');
+    v20 = await new blynk.VirtualPin(20);
+    testPin = await new blynk.VirtualPin(17);
+}
+
+const testBlynk = async () => {
+    console.log("testBlynk");
+
+    testPin.write(1);
+}
+
+const initEvents = async () => {
+    console.log("->initEvents");
+    v20.on('write', function (param) {
+        console.log('V20:', param);
+        if (param == 0)
+            plugTurn(false);
+        if (param == 1)
+            plugTurn(true);
+    });
+
+    v20.on('read', function () {
+        v20.write(new Date().getSeconds());
+    });
+
+    blynk.on('connect', function () {
+        console.log("Blynk ready.");
+        testBlynk();
+    });
+    console.log("initEvents->");
 
 
-v20.on('write', function (param) {
-    console.log('V1:', param);
-});
+   // button.on('action:click', event => console.log('Action', event.action, 'with data', event.data));
+   // sensorHT.on('temperatureChanged', temp => console.log('Temp changed to:', temp));
+}
 
-v20.on('read', function () {
-    v20.write(new Date().getSeconds());
-});
+const run = async () => {
+    //await initGateway();
+    await initBlynk();
+    //await testBlynk();
+    await initEvents();
+    console.log("run->");
+
+    //await plugTurnOn();
+}
+
+run().catch(err => {
+    console.log(err);
+})
