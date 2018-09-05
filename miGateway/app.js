0fea6105c51b4e5a659248891d7c5a9a6fcd4ac7
Andrey Serebrov <andser45@mail.ru>
2018-09-05 10:50:35 +0500

    Changes

--- miGateway/app.js
+++ miGateway/app.js
@@ -105,11 +105,12 @@ const initGateway = () => __awaiter(this, void 0, void 0, function* () {
             const temperature = yield child.temperature();
             console.log('Temperature:', temperature.celsius);
         }
-        //if (child.matches('cap:children')) {
-        //    for (const grandchild of child.children) {
-        //        console.log('grandchild:', grandchild);
-        //    }
-        //}
+        if (child.matches('cap:children')) {
+            const firstOutlet = child.getChild('1'); // depends on the implementation
+            //for (const grandchild of child.children) {
+            //   // console.log('grandchild:', grandchild);
+            //}
+        }
         if (child.matches('cap:battery-level')) {
             console.log('Current battery level:', yield child.batteryLevel());
         }
@@ -231,4 +232,165 @@ run().catch(err => {
     console.log(err);
     console.log("CATCH!");
 });
+function discover() {
+    const browser = miio.browse({
+        cacheTime: 300 // 5 minutes. Default is 1800 seconds (30 minutes)
+    });
+    const devices = {};
+    browser.on('available', reg => {
+        if (!reg.token) {
+            console.log(reg.id, 'hides its token');
+            return;
+        }
+        // Directly connect to the device anyways - so use miio.devices() if you just do this
+        reg.connect()
+            .then(device => {
+            devices[reg.id] = device;
+            // Do something useful with the device
+        })
+            .catch();
+    });
+    browser.on('unavailable', reg => {
+        const device = devices[reg.id];
+        if (!device)
+            return;
+        device.destroy();
+        delete devices[reg.id];
+    });
+}
+//////////////////////*
+//MiioDevice {
+//    model = lumi.gateway.v3,
+//        types = sensor, miio: gateway, miio,
+//            capabilities = illuminance, state, children
+//}
+//MiioDevice {
+//    model = lumi.plug,
+//        types = sensor, power - outlet, power - plug, miio, miio: subdevice, sub - thing,
+//            capabilities = power - consumed, power - load, switchable - power, restorable - state, power, state
+//}
+//miio: 78817862: light
+//GatewayLight {
+//    metadata:
+//    Metadata {
+//        types: Set { 'miio:subdevice', 'miio:gateway-light', 'light', 'sub-thing' },
+//        capabilities:
+//        Set {
+//            'colorable',
+//                'dimmable',
+//                'brightness',
+//                'switchable-power',
+//                'restorable-state',
+//                'power',
+//                'state'
+//        },
+//        actions:
+//        {
+//            color: [Object],
+//                setColor: [Object],
+//                    brightness: [Object],
+//                        setBrightness: [Object],
+//                            increaseBrightness: [Object],
+//                                decreaseBrightness: [Object],
+//                                    power: [Object],
+//                                        setPower: [Object],
+//                                            togglePower: [Object],
+//                                                turnOn: [Object],
+//                                                    turnOff: [Object],
+//                                                        restorableState: [Object],
+//                                                            captureState: [Object],
+//                                                                setState: [Object],
+//                                                                    state: [Object]
+//        },
+//        state: { color: [Object], brightness: [Object], power: [Object] },
+//        events:
+//        {
+//            colorChanged: [Object],
+//                brightnessChanged: [Object],
+//                    powerChanged: [Object],
+//                        stateChanged: [Object]
+//        },
+//        parent: MiioDevice[lumi.gateway.v3],
+//            [Symbol(thing)]: [Circular]
+//    },
+//    model: 'lumi.gateway.v3.light',
+//        internalId: '78817862:light',
+//            gateway: MiioDevice {
+//        model = lumi.gateway.v3,
+//            types = sensor, miio: gateway, miio,
+//                capabilities = illuminance, state, children
+//    },
+//    [Symbol(eventQueue)]: [],
+//        [Symbol(eventEmitter)]:
+//    EventEmitter {
+//        [Symbol(listeners)]: { 'thing:destroyed': [Array] },
+//        [Symbol(anyListeners)]: [],
+//            [Symbol(context)]: [Circular]
+//    },
+//    [Symbol(state)]:
+//    {
+//        power: false,
+//            color: Color { values: [Array], model: 'rgb' },
+//        brightness: 0
+//    },
+//    [Symbol(id)]: 'miio:78817862:light',
+//        [Symbol(defId)]: 'miio:78817862:light',
+//            [Symbol(isDestroyed)]: false,
+//                [Symbol(isInitialized)]: true
+//}
+//miio: 158d00020fecb9
+//MiioDevice {
+//    model = lumi.switch,
+//        types = button, controller, miio, miio: subdevice, sub - thing,
+//            capabilities = battery - level, actions, state
+//}
+//Current battery level: -1
+//miio: 158d0001d571da
+//MiioDevice {
+//    model = lumi.motion,
+//        types = sensor, miio, miio: subdevice, sub - thing,
+//            capabilities = battery - level, motion, state
+//}
+//Current battery level: -1
+//miio: 158d00022712f9
+//MiioDevice {
+//    model = lumi.magnet,
+//        types = sensor, miio, miio: subdevice, sub - thing,
+//            capabilities = battery - level, contact, state
+//}
+//Current battery level: -1
+//miio: 158d0001c2a921
+//MiioDevice {
+//    model = lumi.sensor_ht,
+//        types = sensor, miio, miio: subdevice, sub - thing,
+//            capabilities = relative - humidity, temperature, state
+//}
+//Temperature: 27.29
+//miio: 158d00020f23d5
+//MiioDevice {
+//    model = lumi.plug,
+//        types = sensor, power - outlet, power - plug, miio, miio: subdevice, sub - thing,
+//            capabilities = power - consumed, power - load, switchable - power, restorable - state, power, state
+//}
+//miio: 158d000212f756
+//MiioDevice {
+//    model = lumi.generic.55,
+//        types = miio, miio: subdevice, sub - thing,
+//            capabilities =
+//}
+//miio: 158d0001b7d59e
+//MiioDevice {
+//    model = lumi.generic.15,
+//        types = miio, miio: subdevice, sub - thing,
+//            capabilities =
+//}
+//miio: 158d0002458fc6
+//MiioDevice {
+//    model = lumi.ctrl_neutral2,
+//        types = miio: power -switch, miio, miio:subdevice, wall -switch, sub - thing,
+//        capabilities = children
+//}
+//    Gateway ready!*/
+//subdevices.js
+// 12 : require('./86sw2'), 14 : require('./86sw1'),
 //# sourceMappingURL=app.js.map
